8b780b5c27f6da9561292f89dbeea431f23b968d
ArthurSudbrackIbarra <arthur.ibarra@edu.pucrs.br>
2021-07-10 14:43:52 -0300

    Comentários.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import speech_recognition
 import pyautogui
 import pyperclip
 
-
+# Function responsible for putting selected texts in uppercase, lowercase or titlecase.
 def change_case(option):
+    # Saving user's current clipboard content.
     current_clipboard_content = pyperclip.paste()
+    # Copying selected text.
     pyautogui.hotkey('ctrl', 'c')
+    # Erasing selected text.
     pyautogui.press('backspace')
     if option == 1:
         new_text = pyperclip.paste().upper()
@@ -14,14 +17,19 @@ def change_case(option):
         new_text = pyperclip.paste().lower()
     else:
         new_text = pyperclip.paste().title()
+    # Adding new text to user's clipboard.
     pyperclip.copy(new_text)
+    # Pasting new text.
     pyautogui.hotkey('ctrl', 'v')
+    # Restoring user's clipboard content.
     pyperclip.copy(current_clipboard_content)
 
-
+# Recognizer object.
 recognizer = speech_recognition.Recognizer()
+# Microphone object.
 mic = None
 
+# Opening config.json file and extracting data.
 config_file = open('config.json')
 config_data = json.load(config_file)
 
@@ -31,7 +39,9 @@ uppercase_command = config_data['uppercase_command']
 lowercase_command = config_data['lowercase_command']
 titlecase_command = config_data['titlecase_command']
 
+# Setting up which microphone, language and commands to use.
 if device_index == -1:
+    # Microphone.
     print('You first need to choose which microphone you\'re going to use:\n')
     mic_list = speech_recognition.Microphone.list_microphone_names()
     for i, val in enumerate(mic_list):
@@ -47,6 +57,7 @@ if device_index == -1:
         else:
             str_device_index = input('Invalid option, try again: ')
     mic = speech_recognition.Microphone(device_index=numeric_device_index)
+    # Language and commands.
     language_option = input('\nNow choose your language, 1 for english and 2 for portuguese: ')
     if language_option == '1':
         language = 'en-US'
@@ -58,6 +69,7 @@ if device_index == -1:
         uppercase_command = 'maiúsculo'
         lowercase_command = 'minúsculo'
         titlecase_command = 'título'
+    # Saving configurations to config.json
     to_save = {
         'device_index': device_index,
         'language': language,
@@ -69,9 +81,9 @@ if device_index == -1:
     with open('config.json', 'wb') as jsonFile:
         jsonFile.write(jsonString.encode('utf-8'))
         jsonFile.close()
-
 else:
     try:
+        # Microphone.
         mic = speech_recognition.Microphone(device_index=int(device_index))
     except ValueError:
         print('There was an error with config.json file, download it again at:')
@@ -80,6 +92,7 @@ else:
 
 print('\nNow listening!\n')
 
+# Listening to user audio input.
 with mic as source:
     while True:
         try:
@@ -87,6 +100,7 @@ with mic as source:
             audio = recognizer.listen(source)
             text = recognizer.recognize_google(audio, language=language)
             print(f'You said: {text}')
+            # Determining which case to apply.
             if text == uppercase_command:
                 change_case(1)
             elif text == lowercase_command:
